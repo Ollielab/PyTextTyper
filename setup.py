33e73aab29ff47c73b2d4c13fd847594cc5f0408
Ollielab <oliver.simons123@outlook.com>
2023-01-09 16:03:12 +0000

    changed setuptools

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyTypeWriter-CoolOllie2", # Replace with your own username
+    name="PyTypeWriter", # Replace with your own username
     version="0.0.1",
     author="Ollielab",
     author_email="mail@ollielab.xyz",
